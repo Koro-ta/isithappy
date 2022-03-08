b7560778f193f3c61725311f19420eccade2ddfd
Korota <yutaito1@gmail.com>
2022-03-08 17:28:43 +1100

    Report Issue Test

--- functions.js
+++ functions.js
@@ -2,3 +2,10 @@ function darkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+
+function openForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+function closeForm() {
+  document.getElementById("popupForm").style.display = "none";
+}
