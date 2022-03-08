b2f750059f68aace961fd88a07381bb54b038052
Korota <yutaito1@gmail.com>
2022-03-08 17:39:21 +1100

    Revert "Report Issue Test"
    This reverts commit b7560778f193f3c61725311f19420eccade2ddfd.

--- functions.js
+++ functions.js
@@ -2,10 +2,3 @@ function darkMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
-
-function openForm() {
-  document.getElementById("popupForm").style.display = "block";
-}
-function closeForm() {
-  document.getElementById("popupForm").style.display = "none";
-}
